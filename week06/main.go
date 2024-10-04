ad3c182aa3cf8af687ea7557b0fc15b0e9edc681
eokkka <kioooo8491@naver.com>
2024-10-04 15:48:44 +0900

    v0.6 zerovalue

--- week06/main.go
+++ week06/main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	i := 13
-	var f float64 = 12.9
-	c1 := 'Z'
-	c2 := '김'
-	fmt.Printf("value i : %d, value  f : %f\n", i, f)
-	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
-	fmt.Println(c1, c2)
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
+	var f float64
+	var i int
+	var b bool
+	var s string
+
+	mySchoolAccount := 5
+	fmt.Println(mySchoolAccount)
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f,%t,%s,%d\n", f, b, s, i)
 }
